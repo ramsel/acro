bcd18291efbd51d6bad7a7c4cb1ac22fb5edd3dc
Ramsel Ruiz <sir.ramsel.james@gmail.com>
2015-03-05 09:49:20 -0800

    created project

---
+++ acro/acro/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  acro
+//
+//  Created by Admin on 3/5/15.
+//  Copyright (c) 2015 ramsel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ acro/acro/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  acro
+//
+//  Created by Admin on 3/5/15.
+//  Copyright (c) 2015 ramsel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
